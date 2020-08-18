e1e746e64fb0ec572bb193c45bee33126ba8948b
Sampath Reddy Ette <39610352+sampathreddy01@users.noreply.github.com>
2020-08-18 14:35:36 +0530

    Add files via upload

---
+++ week1/src/main/java/com/assign/App.java
@@ -0,0 +1,8 @@
+package com.in28minutes;
+
+public class App {
+	pubkic String returnsomething() {
+		return "DUMMY";
+	}
+
+}
---
+++ week1/src/test/java/com/assign/AppTest.java
@@ -0,0 +1,10 @@
+package com.in28minutes
+
+import static org.junit.Assert.*;
+
+public class AppTest{
+	@Test 
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
