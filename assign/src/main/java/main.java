7d4e667ab6ac5bb63e5e94936ef7d8cfd8b83c59
Sampath Reddy Ette <39610352+sampathreddy01@users.noreply.github.com>
2020-08-25 14:01:39 +0530

    Add files via upload

---
+++ assign/src/main/java/main.java
@@ -0,0 +1,9 @@
+package income;
+
+public class main{
+	
+	public static void main(String [] args){
+		String filename="C:\\Users\\sampath\\Downloads\\input.csv";
+		reader.read(filename);
+	}
+}
---
+++ assign/src/main/java/reader.java
@@ -0,0 +1,32 @@
+package income;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReader{
+	public static void read(String filename){
+		File file=new File(filename);
+		FileReader fr=new FileReader(file);
+		BufferedReader br=new BufferedReader(br);
+		String del=",";
+		while((line = br.readLine()) != null) {
+	        	if(i==0) i=i+1;
+	        	else
+	        	{
+	        	LogBook.detailLog(line);
+	            tempArr = line.split(delimiter);
+				System.out.println(Integer.parseInt(tempArr[0]));
+	            for(String tempStr : tempArr) {
+	               System.out.print(tempStr + " ");
+					}
+	            System.out.println();
+					}
+	         }
+	         br.close();
+	         } catch(IOException ioe) {
+	            ioe.printStackTrace();
+	         }
+		
+}
+}
